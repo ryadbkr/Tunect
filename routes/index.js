9fabb8308902c3484ce5e9a06b2c2da33176bde8
FawzyOthman <imagino.tn@gmail.com>
2018-05-26 13:02:10 +0100

    first commit fawzy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Notre petite entreprise' ,
+    paragraphe: 'Connait pas la crise',
+    button:'visitez-nous'});
 });
 
 module.exports = router;
